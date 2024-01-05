740e066565f1ce8bda072076caf6dc44c8b82f8c
Ed Gonzales <edgonzales6@gmail.com>
2024-01-05 00:02:42 -0800

    Add new test to address variations

--- README.md
+++ README.md
@@ -15,11 +15,6 @@ Instructions
 - Press the 'Play' button
 <img src="https://i.imgur.com/6YYdSYE.png">
 
-Enhancements
-- Simplify the intro steps to make it easier to parametize
+Future Enhancements
 - Parametize the automation using https://playwright.dev/docs/test-parameterize and https://www.npmjs.com/package/csv-parse
 - Apply POM, creating pages and components
-
-Bugs
-- Nested for-loop is iterating through items that are not part of the results screen
-- Some products do not allow the increase in quantity by more than 1. Apply conditional logic to check for this.
--- tests/automation-task.spec.js
+++ tests/automation-task.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 // ~~~~~~~~ CONSTANTS ~~~~~~~~
 const PRODUCT_NAME = [
   'crocs',
+  'shirt'
 ];
 
 const QUANTITY = [1, 2, 3, 4, 5];
@@ -10,12 +11,12 @@ const QUANTITY = [1, 2, 3, 4, 5];
 // Note to parametize, I would use https://playwright.dev/docs/test-parameterize
 
 // ~~~~~~~~ TEST ~~~~~~~~
-test('verifyAddToCartWorkflow - Socks', async ({ page }) => {
-  
+test('verifyAddToCartWorkflow - Crocs', async ({ page }) => {
+
   // Go to URL, then assert that Chrome goes to the correct URL
   await page.goto('https://www.ebay.com/');
   await expect(page).toHaveURL(/.*ebay/);
-  
+
   // navigate and search for Product Name
   await page.getByRole('link', { name: 'Brand Outlet' }).first().click();
   await page.waitForLoadState();
@@ -25,14 +26,14 @@ test('verifyAddToCartWorkflow - Socks', async ({ page }) => {
   await page.getByPlaceholder('Search Up to 50% off Crocs').click();
   await page.getByPlaceholder('Search Up to 50% off Crocs').fill(PRODUCT_NAME[0]);
   await page.getByPlaceholder('Search Up to 50% off Crocs').press('Enter');
-  
+
   // nested for loop to include getting to the next page and checking that item
   // titles contain the product name
   await page.waitForTimeout(1500);
-  
+
   const paginationItems = page.locator('nav ol li .pagination__item');
   const itemTitles = page.locator('css=.s-item__title');
-  
+
   for (const paginationItem of await paginationItems.all()) {
     await page.waitForLoadState();
     for (const itemTitle of await itemTitles.all()) {
@@ -58,6 +59,75 @@ test('verifyAddToCartWorkflow - Socks', async ({ page }) => {
   // updates the quantity on the drop down to 5, in this example
   const quantityDropDown = page.locator('[data-test-id="list-summary"]').getByRole('combobox')
   await quantityDropDown.selectOption(QUANTITY[4].toString());
+
+  // wait for 1.5 second
+  await page.waitForTimeout(1500);
+  await quantityDropDown.waitFor('attached');
+  await page.waitForLoadState();
+
+  // grab item price, removes the $, and converts it to a float
+  const updatedItemPriceElm = await page.locator('css=.price-details').first().innerText();
+  const updatedItemPriceStr = updatedItemPriceElm.replace("$", "");
+  const updatedItemPriceFloat = parseFloat(updatedItemPriceStr);
+
+  // assertion to confirm the math of when quantity is updated, the price is updated accordingly
+  console.log(`The default item price is $${defaultItemPriceFloat}, and the updated item price is $${updatedItemPriceFloat}.`);
+  expect(updatedItemPriceFloat).toBe(defaultItemPriceFloat * QUANTITY[4]);
+
+  page.close()
+
+});
+
+test('verifyAddToCartWorkflow - Tee, Pant, Tank', async ({ page }) => {
+
+  // Go to URL, then assert that Chrome goes to the correct URL
+  await page.goto('https://www.ebay.com/');
+  await expect(page).toHaveURL(/.*ebay/);
+
+  // navigate and search for Product Name
+  await page.getByRole('link', { name: 'Brand Outlet' }).first().click();
+  await page.waitForLoadState();
+
+  const clothingDropDwn = page.getByRole('button', { name: 'Clothing' });
+  const sideRefinePanelChampion = page.getByLabel('Side Refine Panel').getByRole('link', { name: 'Champion' });
+
+  await clothingDropDwn.click();
+  await sideRefinePanelChampion.click();
+  await page.getByPlaceholder('Search Up to 40% off Champion').click();
+  await page.getByPlaceholder('Search Up to 40% off Champion').fill(PRODUCT_NAME[1]);
+  await page.getByPlaceholder('Search Up to 40% off Champion').press('Enter');
+
+  // nested for loop to include getting to the next page and checking that item
+  // titles contain the product name
+  await page.waitForTimeout(1500);
+
+  const paginationItems = page.locator('nav ol li .pagination__item');
+  const itemTitles = page.locator('css=.s-item__title');
+
+  for (const paginationItem of await paginationItems.all()) {
+    await page.waitForLoadState();
+    for (const itemTitle of await itemTitles.all()) {
+      const lowerCaseText = await itemTitle.innerText().then(text => text.toLowerCase());
+      expect.soft(lowerCaseText).toContain(PRODUCT_NAME[1]);
+    }
+    await page.waitForLoadState();
+    await paginationItem.click();
+  }
+  await itemTitles.last().click();
+
+  // add item to cart
+  await page.getByTestId('x-atc-action').getByTestId('ux-call-to-action').click();
+  await page.getByRole('link', { name: 'Go to cart' }).click();
+
+  // grabs item price, removes the $, and converts it to a float
+  const defaultItemPriceElm = await page.locator('css=.price-details').first().innerText();
+  const defaultItemPriceStr = defaultItemPriceElm.replace("$", "");
+  const defaultItemPriceFloat = parseFloat(defaultItemPriceStr);
+  await page.waitForLoadState();
+
+  // updates the quantity on the drop down to 5, in this example
+  const quantityDropDown = page.locator('[data-test-id="list-summary"]').getByRole('combobox')
+  await quantityDropDown.selectOption(QUANTITY[4].toString())
 
   // wait for 1.5 second
   await page.waitForTimeout(1500);
